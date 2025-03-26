45981dcb15610347b4fcd0b4dc0e586b5e85def8
X-Ray Binary <david.sanchezalb@gmail.com>
2025-03-26 20:09:43 +0100

    Docs Update

--- src/analysis/converter.py
+++ src/analysis/converter.py
@@ -1,6 +1,7 @@
 from .utils import load_json_generalData
 import pandas as pd
 import json
+import re
 
 def converter_method():
     Docs.generate_csv()
@@ -13,9 +14,16 @@ class Docs():
         dict_data = []
         for file in data:
             dict_data.append(file)
-
+        
         df = pd.DataFrame(dict_data)
-        df.to_csv("CSVs/general_data.csv", index=False)
+        df.to_csv("docs/general_data.csv", index=False, encoding="utf-8")
+
+        df = df.where(pd.notnull(df), None)
+        
+        df.columns = [col.replace(" ", "_").replace("[", "").replace("]", "").replace("%", "") for col in df.columns]
+        df.columns = [re.sub(r'\W|^(?=\d)', '_', col) for col in df.columns]
+
+        df.to_xml("docs/general_data.xml", index=False, encoding="utf-8")
 
 
 
